bd2f2a63411f514b96c71082bd8883ef209b0bd1
Alex Fellow <example@example.com>
2025-07-21 14:18:36 +0300

    Add files

---
+++ src/models/expense.model.js
@@ -0,0 +1,35 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../config/database.js';
+
+export class Expense extends Model { }
+
+Expense.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        amount: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: false,
+        },
+        categoryId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            unique: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: false,
+        }
+    },
+    {
+        sequelize,
+        modelName: 'expenses',
+        // "мягкое" удаление
+        paranoid: true,
+    },
+);
