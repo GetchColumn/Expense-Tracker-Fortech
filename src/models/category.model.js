83f4d215572cacca0c0e0372642f41db65b0ec5d
Alex Fellow <example@example.com>
2025-07-24 12:51:17 +0300

    Set TAB to be 2 whitespaces

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -3,22 +3,23 @@ import { sequelize } from '../config/database.js';
 
 export class Category extends Model { }
 
-Category.init(
+Category.init
+  (
     {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
     },
     {
-        sequelize,
-        modelName: 'categories',
-        paranoid: true,
+      sequelize,
+      modelName: 'categories',
+      paranoid: true,
     },
-);
+  );
--- src/models/expense.model.js
+++ src/models/expense.model.js
@@ -4,31 +4,31 @@ import { sequelize } from '../config/database.js';
 export class Expense extends Model { }
 
 Expense.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        sum: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: false,
-        },
-        categoryId: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            unique: false,
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            unique: false,
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        sequelize,
-        modelName: 'expenses',
-        paranoid: true,
+    sum: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
     },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      unique: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false,
+    }
+  },
+  {
+    sequelize,
+    modelName: 'expenses',
+    paranoid: true,
+  },
 );
